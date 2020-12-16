f28ecdde0244bb5e3c734c1cbecdad4f4db34d43
Bruno Braga <bruckbmrj@hotmail.com>
2020-12-16 01:08:35 -0300

    ✨start contato and lanches sketch

---
+++ src/app/pages/lanches/lanches.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LanchesComponent } from './lanches.component';
+
+
+
+@NgModule({
+  declarations: [LanchesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LanchesModule { }
