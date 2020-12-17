5b18234b968263e23ae7368571b51cac45401598
Bruno Braga <bruckbmrj@hotmail.com>
2020-12-16 22:42:02 -0300

    🎨 Applying transparent menu in the home route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'burguer';
+
+  constructor(private router: Router) { }
+
+  getMenuTransparent(){
+    if (this.router.url === '/'){
+      //console.log('estou na home');
+      return "bg-transparent";
+  }
+  return "";
+ }
+
 }
